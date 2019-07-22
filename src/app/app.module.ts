49ec243a39796f2d9bfabb5dc9780e3da894a2f5
Karthik9321 <karthikramachandra93@gmail.com>
2019-07-22 20:19:03 +0530

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,16 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { DemoMaterialModule } from "../material-module";
-import { PeriodicTable } from "./periodic-table/periodic-table.component";
-import { CdkDetailRowDirective } from "../app/periodic-table/cdk-detail-row.directive";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DemoMaterialModule } from '../material-module';
+import { PeriodicTable } from './periodic-table/periodic-table.component';
 
 @NgModule({
-  declarations: [AppComponent, PeriodicTable, CdkDetailRowDirective],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    DemoMaterialModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, PeriodicTable],
+    imports: [BrowserModule, AppRoutingModule, DemoMaterialModule, BrowserAnimationsModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
